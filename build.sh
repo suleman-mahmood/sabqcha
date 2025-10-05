dc4a82d7772c6a40a373811f4a5922af8ba2e7c2
Suleman Mahmood <sulemanmahmood99@gmail.com>
2025-10-06 01:49:26 +0500

    add hetnzer build & deploy scriptp

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Exit immediately if any command fails
+set -e
+
+# Step 1: Go to first directory and run command
+cd ~/Projects/sabqcha/backend
+echo "Running command in $(pwd)..."
+sudo docker build --tag sabqcha_backend --file Dockerfile .
+
+# Step 2: Go to second directory and run command
+cd ~/Projects/sabqcha/frontend
+echo "Running command in $(pwd)..."
+sudo docker build --tag sabqcha_frontend --file Dockerfile .
+
+# Step 3: Go to third directory and run command
+cd ~/Projects
+echo "Running command in $(pwd)..."
+sudo docker compose up -d
+
+echo "✅ All commands executed successfully!"
