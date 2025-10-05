94052b3cb0e86cfd37344a225b9d8a90f94c639b
Suleman Mahmood <sulemanmahmood99@gmail.com>
2025-10-06 00:09:43 +0500

    add endpoint to generate dummy mcqs data

--- backend/api/prompts.py
+++ backend/api/prompts.py
@@ -1,11 +1,27 @@
-# TODO: Fix the pormpts
-
-SYSTEM_PROMPT = """
-bla bla
+MCQ_SYSTEM_PROMPT = """
 """
 
-def generate_user_prompt(tr: str) -> str:
+def generate_mcq_user_prompt(tr: str) -> str:
     return f"""
-    bla bla bal
-    transcription: {tr}
+    Topic: {tr}
+    """
+
+DUMMY_DATA_SYSTEM_PROMPT = """
+You are an expert educational content creator.
+Your task is to generate 10 high-quality multiple-choice questions (MCQs) for a given topic.
+Each question must test conceptual understanding, not rote memorization.
+
+Guidelines:
+Generate exactly 10 questions per topic.
+Each question must have 4 distinct options and only one correct answer.
+Keep the difficulty level to beginner unless specified otherwise.
+All 10 questions should be solvable under 5 mins and don't require a lot of thought and calculations
+Ensure all content is factually accurate and written in clear, simple language.
+Avoid ambiguous, trick, or opinion-based questions.
+Do not include explanations or reasoning — only the JSON output.
+"""
+
+def generate_dummy_data_user_prompt(tr: str) -> str:
+    return f"""
+    Topic: {tr}
     """
--- backend/api/routes/transcribe_routes.py
+++ backend/api/routes/transcribe_routes.py
@@ -8,7 +8,7 @@ from loguru import logger
 from pydantic import BaseModel
 
 from api.models.transcription_models import LlmMcqResponse, TranscriptionDoc
-from api.prompts import SYSTEM_PROMPT, generate_user_prompt
+from api.prompts import DUMMY_DATA_SYSTEM_PROMPT, MCQ_SYSTEM_PROMPT, generate_dummy_data_user_prompt, generate_mcq_user_prompt
 
 
 router = APIRouter(prefix="/transcribe")
@@ -63,8 +63,8 @@ async def transcribe(body: TranscribeBody):
     openai_res = openai_client.responses.parse(
         model="gpt-5-mini",
         input=[
-            {"role": "system", "content": SYSTEM_PROMPT},
-            {"role": "user", "content": generate_user_prompt(transcript)}
+            {"role": "system", "content": MCQ_SYSTEM_PROMPT},
+            {"role": "user", "content": generate_mcq_user_prompt(transcript)}
         ],
         text_format=LlmMcqResponse
     )
@@ -97,3 +97,43 @@ async def get_transcription_mcqs(transcription_id: str):
 
     res = {"mcqs": [m.model_dump(mode="json") for m in trans.mcqs]}
     return JSONResponse(content=res)
+
+@router.get("/create-demo-mcqs")
+async def create_demo_mcqs():
+    from api.main import db, openai_client  # Circular import bs
+
+
+    topics = [
+        # "Newton's law of motion",
+        # "Work, Engergy, Power",
+        # "Waves and Sound",
+        # "Quadratic Equations and Functions"
+        # "Trigonometric Identities and Applications"
+        "Distributed Systems",
+        "Computer Graphics",
+        "Computer Vision",
+        "Business Communication",
+        "Psychology",
+    ]
+
+    for topic in topics:
+        # Define the messages
+        openai_res = openai_client.responses.parse(
+            model="gpt-5-mini",
+            input=[
+                {"role": "system", "content": DUMMY_DATA_SYSTEM_PROMPT},
+                {"role": "user", "content": generate_dummy_data_user_prompt(topic)}
+            ],
+            text_format=LlmMcqResponse
+        )
+        llm_res = openai_res.output_parsed
+        if not llm_res:
+            logger.error("Invalid Response form OpenAI: {}", openai_res.model_dump(mode="json"))
+            return Response("Invalid response from OpenAI", status_code=400)
+
+        doc = TranscriptionDoc(
+            audio_file_path="dummy.mp3",
+            transcribed_content="dummy-data",
+            mcqs=llm_res.mcqs,
+        )
+        db.collection("transcription").add(doc.model_dump(mode="json"))
