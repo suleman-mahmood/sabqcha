ae0ff46d4931eadf60233b41d5f6810a35a88afa
Suleman Mahmood <sulemanmahmood99@gmail.com>
2025-10-07 14:28:02 +0500

    split audio into small files and transcribe them one by one

--- backend/api/routes/transcribe_routes.py
+++ backend/api/routes/transcribe_routes.py
@@ -1,4 +1,6 @@
 import os
+import math
+import ffmpeg
 from fastapi.responses import JSONResponse
 from google.cloud.firestore import Client
 from google.cloud.storage import Bucket
@@ -43,42 +45,85 @@ async def transcribe(
     openai_client: OpenAI = Depends(get_openai_client),
     bucket: Bucket = Depends(get_bucket),
 ):
-    with tempfile.NamedTemporaryFile(suffix=".mp3") as temp_file:
+    all_transcripts: list[str] = []
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        input_file = tempfile.NamedTemporaryFile(suffix=".mp3", dir=temp_dir, delete=False)
+
         blob = bucket.blob(body.file_path)
-        blob.download_to_filename(temp_file.name)
+        blob.download_to_filename(input_file.name)
 
-        files = {"file": ("audio.mp3", temp_file, "audio/mpeg")}
-        data = {"model": "scribe", "language": "ur"}
-        headers = {"Authorization": f"Bearer {UPLIFT_API_KEY}"}
+        chunk_length = 60
+        probe = ffmpeg.probe(input_file.name)
+        duration = math.floor(float(probe["format"]["duration"]))
+        num_chunks = math.ceil(duration / chunk_length)
+        chunks = []
 
-        response = requests.post(
-            f"{UPLIFT_BASE_URL}/transcribe/speech-to-text", headers=headers, files=files, data=data
-        )
+        logger.info("Total audio duration: {}", duration)
+        logger.info("Num chunks: {}", num_chunks)
 
-    logger.info("Response Status: {}", response.status_code)
+        # If last chunk is very small ignore it
+        if duration - (num_chunks - 1) * chunk_length < 5:
+            num_chunks -= 1
 
-    res_json = response.json()
-    logger.info("Json: {}", res_json)
+        for i in range(num_chunks):
+            start_time = i * chunk_length
+            temp_chunk = tempfile.NamedTemporaryFile(
+                suffix=os.path.splitext(input_file.name)[1],
+                dir=temp_dir,
+                delete=False,
+            )
 
-    if response.status_code != 200:
-        logger.error("Uplift API Failed")
-        return Response("Uplift API Failed", status_code=400)
+            # Create chunk using ffmpeg (lossless copy)
+            (
+                ffmpeg.input(input_file.name, ss=start_time, t=chunk_length)
+                .output(temp_chunk.name, c="copy")
+                .overwrite_output()
+                .run(quiet=True)
+            )
 
-    if "transcript" not in res_json:
-        logger.error("No transcribtions in response")
-        return Response("Uplift API Failed", status_code=400)
+            chunks.append(temp_chunk)
 
-    transcript = res_json["transcript"]
+        for sharded_file in chunks:
+            logger.info("Sending file {} to API", sharded_file.name)
 
+            files = {"file": ("audio.mp3", sharded_file, "audio/mpeg")}
+            data = {"model": "scribe-mini", "language": "ur"}
+            headers = {"Authorization": f"Bearer {UPLIFT_API_KEY}"}
+
+            response = requests.post(
+                f"{UPLIFT_BASE_URL}/transcribe/speech-to-text",
+                headers=headers,
+                files=files,
+                data=data,
+            )
+
+            logger.info("Response Status: {}", response.status_code)
+
+            res_json = response.json()
+            logger.info("Json: {}", res_json)
+
+            if response.status_code != 200:
+                logger.error("Uplift API returned non 200 error: {}", response.status_code)
+                return Response("Uplift API Failed", status_code=400)
+
+            if "transcript" not in res_json:
+                logger.error("No transcript in response")
+                return Response("Uplift API Failed", status_code=400)
+
+            transcript = res_json["transcript"]
+            all_transcripts.append(transcript)
+
+    final_transcript = " ".join(all_transcripts)
     trans_id = await transcription_db.insert_transcription(
-        cur, body.file_path, body.title, body.user_id, transcript
+        cur, body.file_path, body.title, body.user_id, final_transcript
     )
 
     openai_res = openai_client.responses.parse(
         model="gpt-5-mini",
         input=[
             {"role": "system", "content": MCQ_SYSTEM_PROMPT},
-            {"role": "user", "content": generate_mcq_user_prompt(transcript)},
+            {"role": "user", "content": generate_mcq_user_prompt(final_transcript)},
         ],
         text_format=LlmMcqResponse,
     )
