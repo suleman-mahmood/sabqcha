2bd94bf836fe955b03cac026ae26349480e61910
Suleman Mahmood <sulemanmahmood99@gmail.com>
2025-11-15 11:10:55 +0500

    add analytics

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -1,18 +1,20 @@
 // lib/firebase.ts
 import { initializeApp, getApps } from "firebase/app";
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCyi2LGMg8GV_1UXNUj5A12yE5TFgRjTmo",
-  authDomain: "sabqcha.firebaseapp.com",
-  projectId: "sabqcha",
-  storageBucket: "sabqcha.firebasestorage.app",
-  messagingSenderId: "888570058051",
-  appId: "1:888570058051:web:36e10d682642848c754bf6",
+    apiKey: "AIzaSyCyi2LGMg8GV_1UXNUj5A12yE5TFgRjTmo",
+    authDomain: "sabqcha.firebaseapp.com",
+    projectId: "sabqcha",
+    storageBucket: "sabqcha.firebasestorage.app",
+    messagingSenderId: "888570058051",
+    appId: "1:888570058051:web:36e10d682642848c754bf6",
+    measurementId: "G-T8KEJCK3XD",
 };
 
 // Initialize Firebase only once
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
 
-// Get Storage reference
 export const storage = getStorage(app);
+export const analytics = getAnalytics(app);
